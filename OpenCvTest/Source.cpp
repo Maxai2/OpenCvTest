fd39b2ab24025431511324966849431be322a574
Хамелеон <maxali946@gmail.com>
2018-01-12 17:32:10 +0400

    50%

--- OpenCvTest/Source.cpp
+++ OpenCvTest/Source.cpp
@@ -603,6 +603,170 @@
 //}
 
 ////--------------------------------------
+//#include <cv.h>
+//#include <highgui.h>
+//#include <stdlib.h>
+//#include <stdio.h>
+//
+//IplImage* image = 0;
+//IplImage* dst = 0;
+//
+//int main(int argc, char* argv[])
+//{
+//	// имя картинки задется первым параметром
+//	const char* filename = argc >= 2 ? argv[1] : "cat.jpg";
+//	char windowName[] = "original";
+//	char noiseWindow[] = "noise";
+//
+//	// получаем картинку
+//	image = cvLoadImage(filename, 1);
+//	assert(image != 0);
+//
+//	// клонируем объект
+//	dst = cvCloneImage(image);
+//
+//	int count = 0;
+//
+//	// окно для отображения картинки
+//	cvNamedWindow(windowName, CV_WINDOW_AUTOSIZE);
+//	cvNamedWindow(noiseWindow, CV_WINDOW_AUTOSIZE);
+//
+//	// инициализация состояния 
+//	CvRNG rng = cvRNG(0xffffffff);
+//
+//	// пробегаемся по всем пикселям изображения
+//	for (int y = 0; y < dst->height; y++)
+//	{
+//		uchar* ptr = (uchar*)(dst->imageData + y * dst->widthStep);
+//
+//		for (int x = 0; x < dst->width; x++)
+//		{
+//			if (cvRandInt(&rng) % 100 >= 97)
+//			{
+//				// 3 канала
+//				ptr[3 * x] = cvRandInt(&rng) % 255; // B
+//				ptr[3 * x + 1] = cvRandInt(&rng) % 255; // G
+//				ptr[3 * x + 2] = cvRandInt(&rng) % 255; // R
+//				count++;
+//
+//				/*
+//				// красные пиксели
+//				ptr[3 * x] = 0;
+//				ptr[3 * x + 1] = 0;
+//				ptr[3 * x + 2] = 255;
+//				*/
+//			}
+//		}
+//	}
+//
+//	printf("[i] noise: %d (%.2f%c)\n", count, (float)count / (dst->height * dst->width) * 100, '%');
+//
+//	// показываем картинку
+//	cvShowImage(windowName, image);
+//	cvShowImage(noiseWindow, dst);
+//
+//	// ждем нажатия клавиши
+//	cvWaitKey(0);
+//
+//	// освободжаем ресурс
+//	cvReleaseImage(&image);
+//	cvReleaseImage(&dst);
+//	// удаляем окна
+//	cvDestroyAllWindows();
+//	return 0;
+//}
+
+////--------------------------------------
+
+//#include <cv.h>
+//#include <highgui.h>
+//#include <stdlib.h>
+//#include <stdio.h>
+//
+//IplImage* image = 0;
+//IplImage* dst = 0;
+//
+//IplImage* erode = 0;
+//IplImage* dilate = 0;
+//
+//int radius = 1;
+//int radius_max = 10;
+//
+////
+//// функция-обработчик ползунка-
+//// радиус ядра
+//void myTrackBbarRadius(int pos) { radius = pos; }
+//
+//int iterations = 1;
+//int iterations_max = 10;
+//
+////
+//// функция-обработчик ползунка-
+//// число итераций
+//void myTrackBarIterations(int pos) { iterations = pos; }
+//
+//int main(int argc, char* argv[])
+//{
+//	// имя картинки задется первым параметром
+//	const char* filename = argc == 2 ? argv[1] : "cat.jpg";
+//	char windowName[] = "origin";
+//	char erodeWindow[] = "erode";
+//	char dilateWindow[] = "dilate";
+//
+//	// получаем картинку
+//	image = cvLoadImage(filename, 1);
+//	// клонируем объект
+//	dst = cvCloneImage(image);
+//	erode = cvCloneImage(image);
+//	dilate = cvCloneImage(image);
+//
+//	printf("[i] image: %s\n", filename);
+//	assert(image != 0);
+//
+//	//окно для отображения картинки
+//	cvNamedWindow(windowName, CV_WINDOW_AUTOSIZE);
+//	cvNamedWindow(erodeWindow, CV_WINDOW_AUTOSIZE);
+//	cvNamedWindow(dilateWindow, CV_WINDOW_AUTOSIZE);
+//
+//	cvCreateTrackbar("Radius", windowName, &radius, radius_max, myTrackBbarRadius);
+//	cvCreateTrackbar("Iteration", windowName, &iterations, iterations_max, myTrackBarIterations);
+//
+//	while (true)
+//	{
+//		// показываем картинку
+//		cvShowImage(windowName, image);
+//
+//		// создаем ядро
+//		IplConvKernel* Kern = cvCreateStructuringElementEx(radius * 2 + 1, radius * 2 + 1, radius, radius, CV_SHAPE_ELLIPSE);
+//
+//		// выполняем преобразование
+//		cvErode(image, erode, Kern, iterations);
+//		cvDilate(image, dilate, Kern, iterations);
+//
+//		// показываем результат
+//		cvShowImage(erodeWindow, erode);
+//		cvShowImage(dilateWindow, dilate);
+//
+//		// удаляем ядро
+//		cvReleaseStructuringElement(&Kern);
+//
+//		char c = cvWaitKey(33);
+//		if (c == 27)
+//			break;
+//	}
+//
+//	// освобождаем ресурсы
+//	cvReleaseImage(&image);
+//	cvReleaseImage(&dst);
+//	cvReleaseImage(&erode);
+//	cvReleaseImage(&dilate);
+//	// удаляем окна
+//	cvDestroyAllWindows();
+//	return 0;
+//}
+
+////--------------------------------------
+
 #include <cv.h>
 #include <highgui.h>
 #include <stdlib.h>
@@ -611,72 +775,8 @@
 IplImage* image = 0;
 IplImage* dst = 0;
 
-int main(int argc, char* argv[])
-{
-	// имя картинки задется первым параметром
-	const char* filename = argc >= 2 ? argv[1] : "cat.jpg";
-	char windowName[] = "original";
-	char noiseWindow[] = "noise";
+int radius = 1;
+int radius_max = 10;
 
-	// получаем картинку
-	image = cvLoadImage(filename, 1);
-	assert(image != 0);
-
-	// клонируем объект
-	dst = cvCloneImage(image);
-
-	int count = 0;
-
-	// окно для отображения картинки
-	cvNamedWindow(windowName, CV_WINDOW_AUTOSIZE);
-	cvNamedWindow(noiseWindow, CV_WINDOW_AUTOSIZE);
-
-	// инициализация состояния 
-	CvRNG rng = cvRNG(0xffffffff);
-
-	// пробегаемся по всем пикселям изображения
-	for (int y = 0; y < dst->height; y++)
-	{
-		uchar* ptr = (uchar*)(dst->imageData + y * dst->widthStep);
-
-		for (int x = 0; x < dst->width; x++)
-		{
-			if (cvRandInt(&rng) % 100 >= 97)
-			{
-				// 3 канала
-				ptr[3 * x] = cvRandInt(&rng) % 255; // B
-				ptr[3 * x + 1] = cvRandInt(&rng) % 255; // G
-				ptr[3 * x + 2] = cvRandInt(&rng) % 255; // R
-				count++;
-
-				/*
-				// красные пиксели
-				ptr[3 * x] = 0;
-				ptr[3 * x + 1] = 0;
-				ptr[3 * x + 2] = 255;
-				*/
-			}
-		}
-	}
-
-	printf("[i] noise: %d (%.2f%c)\n", count, (float)count / (dst->height * dst->width) * 100, '%');
-
-	// показываем картинку
-	cvShowImage(windowName, image);
-	cvShowImage(noiseWindow, dst);
-
-	// ждем нажатия клавиши
-	cvWaitKey(0);
-
-	// освободжаем ресурс
-	cvReleaseImage(&image);
-	cvReleaseImage(&dst);
-	// удаляем окна
-	cvDestroyAllWindows();
-	return 0;
-}
 
 ////--------------------------------------
-
-#include <cv.h>
-#include <highgui.h>
